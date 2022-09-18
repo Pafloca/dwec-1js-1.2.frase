cea0869cd85ce22612dd89b0c00ed14903bc27ea
Pau Florencio <pauflorencio123@example.com>
2022-09-18 13:07:23 +0200

    Test realizados

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,60 @@
 'use strict'
 
 function letras(cadena) {
-
+	return cadena.length;
 }
 
 function palabras(cadena) {
-
+	let palabras = cadena.split(' ');
+	return palabras.length;
 }
 
 function maysc(cadena) {
-
+	return cadena.toLocaleUpperCase();
 }
 
 function titulo(cadena) {
+	let palabras = cadena.split(' ');
+
+	let a = [];
+
+	for(let i = 0; palabras.length > i; i++) {
+		a.push(palabras[i].charAt(0).toLocaleUpperCase() + palabras[i].substring(1));
+	}
+
+	let frase = a.join(' ');
+
+	return frase;
 
 }
 
 function letrasReves(cadena) {
 
+	let letrasReves = cadena.split('').reverse().join('');
+
+	return letrasReves;
 }
 
 function palabrasReves(cadena) {
+	let palabrasReves = cadena.split(' ').reverse().join(' ');
 
+	return palabrasReves;
 }
 
 function palindromo(cadena) {
 
+	let frase = cadena.replace(/ /g, "").toLocaleLowerCase();
+
+	let x = 1;
+
+	for(let i = 0; i < frase.length; i++) {
+		if(frase[i] != frase[frase.length - x]) {
+			return false;
+		}
+		x++;
+	}
+
+	return true;
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
 'use strict'
 
+let fraseUsuario = "la ruta nos aporto otro paso natural";
 
+console.log(letras(fraseUsuario) + ' letras y ' + palabras(fraseUsuario) + ' palabras');
+
+console.log(maysc(fraseUsuario));
+
+console.log(titulo(fraseUsuario));
+
+console.log(letrasReves(fraseUsuario));
+
+console.log(palabrasReves(fraseUsuario));
+
+if (palindromo(fraseUsuario)) {
+    console.log('Si es un palindromo');
+} else {
+    console.log('No es un palindromo');
+}
